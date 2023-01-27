083086e650cc1b1c4faadaf9c03029c55b2a4dc4
JYH <salvayh@thingspire.com>
2023-01-27 11:38:27 +0900

    step 추가/ instance (잡) - execution (이력) 이해

--- src/main/java/com/junsun/springbatch/job/SimpleJobConfiguration.java
+++ src/main/java/com/junsun/springbatch/job/SimpleJobConfiguration.java
@@ -3,11 +3,12 @@ package com.junsun.springbatch.job;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -18,20 +19,43 @@ public class SimpleJobConfiguration {
     private final JobBuilderFactory jobBuilderFactory; // 생성자 DI 받음
     private final StepBuilderFactory stepBuilderFactory; // 생성자 DI 받음
 
+    //job - job instance - job execution
+    
     @Bean
     public Job simpleJob() {
         return jobBuilderFactory.get("simpleJob")
-                .start(simpleStep1())
+                .start(simpleStep1(null))
+                .next(simpleStep2(null))
                 .build();
     }
 
     @Bean
-    public Step simpleStep1() {
+    @JobScope
+    //Job Parameter로 받은 값을 로그에 추가로 출력시키는 기능
+    //Program arguments에 requestDate=20180805 추가후 BATCH_JOB_INSTANCE 확인
+    //BATCH_JOB_INSTANCE - BATCH_JOB_EXECUTION 부모-자식 (잡 - 잡의 성공/실패내역)
+    public Step simpleStep1(@Value("#{jobParameters[requestDate]}") String requestDate) {
         return stepBuilderFactory.get("simpleStep1")
                 .tasklet((contribution, chunkContext) -> {
                     log.info(">>>>> This is Step1");
+                    log.info(">>>>> requestDate = {}", requestDate);
+                    return RepeatStatus.FINISHED;
+                    //throw new IllegalArgumentException("step1 실패" + requestDate);
+                })
+                .build();
+    }
+    
+    //동일한 Job Parameter로 2번 실행했는데 같은 파라미터로 실행되었다는 에러가 발생하지 않았다
+    //Spring Batch는 동일한 Job Parameter로 성공한 기록이 있을때만 재수행이 안된다
+    public Step simpleStep2(@Value("#{jobParameters[requestDate]}") String requestDate) {
+        return stepBuilderFactory.get("simpleStep2")
+                .tasklet((contribution, chunkContext) -> {
+                    log.info(">>>>> This is Step2");
+                    log.info(">>>>> requestDate = {}", requestDate);
                     return RepeatStatus.FINISHED;
                 })
                 .build();
     }
+    
+   
 }
